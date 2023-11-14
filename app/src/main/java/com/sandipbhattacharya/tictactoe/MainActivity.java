91fde51d5fa49f2c4c9f9527a09e9cf25c948f01
chris <christopherma597@gmail.com>
2023-11-14 16:47:16 -0600

    still trying to figure out why the screens won't switch

--- app/src/main/java/com/sandipbhattacharya/tictactoe/MainActivity.java
+++ app/src/main/java/com/sandipbhattacharya/tictactoe/MainActivity.java
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void switchScreens(View v){
-        Intent intent = new Intent(this,TikTakTowPlay.class);
-        startActivity(intent);
+        Button goToAnotherClass =  findViewById(R.id.buttonPlay);
+        goToAnotherClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TikTakTowPlay.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
